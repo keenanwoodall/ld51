1c1b34a7db139b5dcbb27601bb93999d35e83999
Keenan Woodall <keenanwoodall@gmail.com>
2022-10-02 15:44:25 -0500

    Added lightning fx indicator to sword

--- Assets/Scripts/TimeChangedEvent.cs
+++ Assets/Scripts/TimeChangedEvent.cs
@@ -3,18 +3,29 @@ using UnityEngine.Events;
 
 public class TimeChangedEvent : MonoBehaviour
 {
+    public float customTime = 9f;
     public UnityEvent<float> timeChanged;
     public UnityEvent<float> timeChangedNormalized;
+    public UnityEvent customTimePassed;
     public UnityEvent tenSecondsPassed;
 
-    private void Awake()
+    private bool _customTimeSometin;
+
+    private void Start()
     {
         GameManager.Instance.TenSecondsPassed += tenSecondsPassed.Invoke;
+        GameManager.Instance.TenSecondsPassed += () => _customTimeSometin = false;
     }
 
     private void Update()
     {
         timeChanged.Invoke(GameManager.CurrentTime);
         timeChangedNormalized.Invoke(GameManager.CurrentTime / 10f);
+
+        if (!_customTimeSometin && GameManager.CurrentTime > customTime)
+        {
+            _customTimeSometin = true;
+            customTimePassed.Invoke();
+        }
     }
 }
